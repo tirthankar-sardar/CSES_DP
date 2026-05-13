93fcb5958c21f76035b5a68c3db119b695016032
Tirthankar Sardar <tirthankar2103@gmail.com>
2026-05-13 10:13:12 +0530

    Add files via upload

---
+++ coin_combinations_1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <map>
+#include <vector>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <string>
+#include <cmath>
+#include <chrono>
+#include <bits/stdc++.h>
+#define MOD 1000000007
+using namespace std;
+
+int main(){
+    int n, x;
+    cin >> n >> x;
+    vector<int> coins(n);
+    for(int i = 0; i < n; i++){
+        cin >> coins[i];
+    }
+    vector<long long> dp(x + 1, 0);
+    dp[0] = 1;
+    for(int i = 1; i <= x; i++){
+        for(int j = 0; j < n; j++){
+            if(coins[j] <= i){
+                dp[i] = (dp[i] + dp[i - coins[j]]) % MOD;
+            }
+        }
+    }
+    cout << dp[x] << endl;
+}
